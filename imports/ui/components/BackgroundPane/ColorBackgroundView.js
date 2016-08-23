69c8629089e7eedcb5b02c547a72aa0abed39e57
Robin Willis <robincwillis@gmail.com>
2016-08-22 23:53:11 -0400

    kill color picker comments

--- imports/ui/components/BackgroundPane/ColorBackgroundView.js
+++ imports/ui/components/BackgroundPane/ColorBackgroundView.js
@@ -5,8 +5,7 @@ import CheckIcon from '../../assets/icons/check.js';
 
 //Components
 import Button from '../Common/Button.js';
-//ColorPicker is missing
-// import ColorPicker from 'react-colorpickr';
+
 
 // import '../../sass/components/common/color-picker.scss';
 import '../../sass/components/common/inputs.scss';
